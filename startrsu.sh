788596fc4835b0108575c38ca0c9416dd53c5347
Nomikose <Dartwer55@gmail.com>
2022-11-10 21:55:52 +0300

    Поднять РСУ для работы

---
+++ startrsu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd ~
+mydir=$(pwd)
+cd /home/root/ext-fs/
+echo "$mydir"
+./set_environment.sh && cd /home/root/tests/
+./rsu_main_1.9.0 && sleep 2
+killall rsu_main_1.9.0 && sleep 2
+cd ~
+cd /home/root/ext-fs/usr/cv2x_config/
+cp *Cfg /home/root/tests/cfgRsu_1.9.0/
+cd /home/root/tests/
+echo "$mydir"
+./rsu_main_1.9.0
